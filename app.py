ce8ddd0034b8e68a9e8e64d92fffeb4050335fa2
Abdullah Kazi <81045582+Abdullah-Kazi@users.noreply.github.com>
2024-05-28 13:56:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -132,8 +132,7 @@ uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
 
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
-    st.write("Uploaded Data Preview (first 5 rows):")
-    st.write(data.head())
+
 
     if st.sidebar.button('Predict Uploaded Data'):
         try:
