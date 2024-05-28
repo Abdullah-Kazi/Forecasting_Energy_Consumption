4836e8dcf3855e17e70c3e02949326f5d7195bd4
Abdullah Kazi <81045582+Abdullah-Kazi@users.noreply.github.com>
2024-05-28 15:41:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -24,12 +24,15 @@ st.write("""
 def load_models():
     models = {}
     try:
-        models['XGBoost_model.pkl'] = xgb.XGBRegressor()
-        models['Prophet_model.pkl'] = ProphetRegressor()
-        models['LightGBM_model.pkl'] = lightgbm.LGBMRegressor()
+        model_names = ['XGBoost_model', 'Linear Regression_model', 'LightGBM_model']
+        for model_name in model_names:
+            with open(f'{model_name}.pkl', 'rb') as file:
+                models[model_name] = pickle.load(file)
     except Exception as e:
         st.error(f"Failed to load model due to: {e}")
     return models
+
+models = load_models()
 
 models = load_models()
 
